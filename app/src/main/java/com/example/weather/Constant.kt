68b01a25179522db029a3a8b7bbf263e81e25a0a
叶梦璃愁 <swk2460050897@outlook.com>
2022-11-01 11:12:40 +0800

    init commit

--- app/src/main/java/com/example/weather/Constant.kt
+++ app/src/main/java/com/example/weather/Constant.kt
@@ -1,8 +1,19 @@
 package com.example.weather
 
+import com.qweather.sdk.view.HeConfig
+import com.qweather.sdk.view.QWeather
+import kotlinx.coroutines.flow.flow
+
 object Constant {
-
-
+    // api id
     const val API_PUBLIC_ID = "HE2210311349181928"
+    //api key
+    const val API_PRIVATE_KEY = ""
+
+    init {
+        //全局执行一次初始化
+        HeConfig.init("PublicId", "PrivateKey")
+        
+    }
 
 }
